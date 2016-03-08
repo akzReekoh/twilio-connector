daedbbc0cada9eb058b3085b0c7639235e05683c
akzReekoh <achilles.dinglasan@p3ople4u.com>
2016-03-08 18:36:10 +0800

    REEKOH-107
    -Updated travis.yml
    -Updated app.js to accept array input
    -Bumped version
    -Updated test suite to test array input

--- app.js
+++ app.js
@@ -1,74 +1,76 @@
 'use strict';
 
 var isEmpty       = require('lodash.isempty'),
+	isArray = require('lodash.isarray'),
+	async = require('async'),
 	isPlainObject = require('lodash.isplainobject'),
 	platform      = require('./platform'),
 	twilioClient  = require('twilio'),
 	config;
 
-/*
- * Listen for the data event.
- */
+let sendData = (data) => {
+	var to, from, body;
+
+	if (isEmpty(data.to))
+		to = config.default_receiver;
+	else
+		to = data.to;
+
+	if (isEmpty(data.from))
+		from = config.default_sender;
+	else
+		from = data.from;
+
+	if (isEmpty(data.body))
+		body = config.body;
+	else
+		body = data.body;
+
+	delete data.to;
+	delete data.from;
+	delete data.body;
+
+	var params = {
+		to: to,
+		from: from
+	};
+
+	if (isEmpty(body))
+		params.body = JSON.stringify(data, null, 4);
+	else
+		params.body = body + '\n\n' + JSON.stringify(data, null, 4);
+
+	twilioClient.sendMessage(params, function (error) {
+		if (error) {
+			console.error(error);
+			platform.handleException(error);
+		}
+		else {
+			platform.log(JSON.stringify({
+				title: 'Twilio SMS Sent',
+				data: params
+			}));
+		}
+	});
+};
+
 platform.on('data', function (data) {
 	if (isPlainObject(data)) {
-		var to, from, body;
-
-		if (isEmpty(data.to))
-			to = config.default_receiver;
-		else
-			to = data.to;
-
-		if (isEmpty(data.from))
-			from = config.default_sender;
-		else
-			from = data.from;
-
-		if (isEmpty(data.body))
-			body = config.body;
-		else
-			body = data.body;
-
-		delete data.to;
-		delete data.from;
-		delete data.body;
-
-		var params = {
-			to: to,
-			from: from
-		};
-
-		if (isEmpty(body))
-			params.body = JSON.stringify(data, null, 4);
-		else
-			params.body = body + '\n\n' + JSON.stringify(data, null, 4);
-
-		twilioClient.sendMessage(params, function (error) {
-			if (error) {
-				console.error(error);
-				platform.handleException(error);
-			}
-			else {
-				platform.log(JSON.stringify({
-					title: 'Twilio SMS Sent',
-					data: params
-				}));
-			}
+		sendData(data);
+	}
+	else if(isArray(data)){
+		async.each(data, (datum) => {
+			sendData(datum);
 		});
 	}
 	else
-		platform.handleException(new Error('Invalid data received. Must be a valid JSON Object. Data ' + data));
+		platform.handleException(new Error('Invalid data received. Must be a valid Array/JSON Object. Data ' + data));
 });
 
-/*
- * Event to listen to in order to gracefully release all resources bound to this service.
- */
 platform.on('close', function () {
 	platform.notifyClose();
 });
 
-/*
- * Listen for the ready event.
- */
 platform.once('ready', function (options) {
 	twilioClient = require('twilio')(options.account_sid, options.auth_token);
 	config = options;
